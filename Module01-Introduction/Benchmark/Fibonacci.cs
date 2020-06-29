45f766f0ec0da3eda21252516be37bfb5e8f828a
Maciej Piwowarczyk <maciej.piwowarczyk@relativity.com>
2020-06-29 22:55:03 +0200

    solution homework 01

--- Module01-Introduction/Benchmark/Fibonacci.cs
+++ Module01-Introduction/Benchmark/Fibonacci.cs
@@ -4,6 +4,7 @@ using BenchmarkDotNet.Attributes;
 namespace Dotnetos.AsyncExpert.Homework.Module01.Benchmark
 {
     [DisassemblyDiagnoser(exportCombinedDisassemblyReport: true)]
+    [MemoryDiagnoser]
     public class FibonacciCalc
     {
         // HOMEWORK:
@@ -26,14 +27,35 @@ namespace Dotnetos.AsyncExpert.Homework.Module01.Benchmark
         [ArgumentsSource(nameof(Data))]
         public ulong RecursiveWithMemoization(ulong n)
         {
-            return 0;
+            if (n == 1 || n == 2) return 1;
+            var memo = new ulong[n - 2];
+            return RecursiveWithMemoizationImpl(n - 1, memo) + RecursiveWithMemoizationImpl(n - 2, memo);
         }
-        
+
+        private ulong RecursiveWithMemoizationImpl(ulong n, ulong[] memo)
+        {
+            if (n == 1 || n == 2) return 1;
+            var cached = memo[n - 3];
+            if (cached != 0) return cached;
+            var res = RecursiveWithMemoizationImpl(n - 1, memo) + RecursiveWithMemoizationImpl(n - 2, memo);
+            memo[n - 3] = res;
+            return res;
+        }
+
         [Benchmark]
         [ArgumentsSource(nameof(Data))]
         public ulong Iterative(ulong n)
         {
-            return 0;
+            if (n == 1 || n == 2) return 1;
+
+            ulong a = 1, b = 1, c = 0;
+            for (uint i = 2; i < n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return c;
         }
 
         public IEnumerable<ulong> Data()
--- Module01-Introduction/Benchmark/Program.cs
+++ Module01-Introduction/Benchmark/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using BenchmarkDotNet.Running;
 
 namespace Dotnetos.AsyncExpert.Homework.Module01.Benchmark
@@ -7,6 +8,11 @@ namespace Dotnetos.AsyncExpert.Homework.Module01.Benchmark
     {
         static void Main(string[] args)
         {
+            //var calc = new FibonacciCalc();
+            //for (uint i = 1; i < 10; i++)
+            //{
+            //    Console.WriteLine($"[{i}] = {calc.Recursive(i)} / {calc.RecursiveWithMemoization(i)} / {calc.Iterative(i)}");
+            //}
             BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
         }
     }
