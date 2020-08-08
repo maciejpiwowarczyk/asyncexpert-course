1b6f9b941e0c25e059fe542a7741dae5120209e7
Maciej Piwowarczyk <maciej.piwowarczyk@relativity.com>
2020-08-08 22:55:22 +0200

    module 04 awaitables homework

--- Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
+++ Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
@@ -5,10 +5,25 @@ namespace AwaitableExercises.Core
 {
     public static class BoolExtensions
     {
+        public static BoolAwaiter GetAwaiter(this bool boolVal)
+        {
+            return new BoolAwaiter(boolVal);
+        }
     }
 
     public class BoolAwaiter : INotifyCompletion
     {
+        private readonly bool _boolVal;
+
+        public BoolAwaiter(bool boolVal)
+        {
+            _boolVal = boolVal;
+        }
+
+        public bool IsCompleted => true;
+
+        public bool GetResult() => _boolVal;
+
         public void OnCompleted(Action continuation)
         {
             throw new NotImplementedException();
