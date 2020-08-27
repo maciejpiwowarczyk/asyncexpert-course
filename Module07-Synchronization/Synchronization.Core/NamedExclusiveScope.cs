651750a07939d390ded96e2cdcdfc9c447b5956e
Maciej Piwowarczyk <maciej.piwowarczyk@relativity.com>
2020-08-27 23:16:56 +0200

    module 07 homework

--- Module07-Synchronization/Synchronization.Core/NamedExclusiveScope.cs
+++ Module07-Synchronization/Synchronization.Core/NamedExclusiveScope.cs
@@ -11,8 +11,33 @@ namespace Synchronization.Core
      */
     public class NamedExclusiveScope : IDisposable
     {
+        private readonly Semaphore _semaphore;
+
         public NamedExclusiveScope(string name, bool isSystemWide)
         {
+            var createdNew = false;
+            if (isSystemWide)
+            {
+                _semaphore = new Semaphore(1, 1, $"Global\\{name}", out createdNew);
+            }
+            else
+            {
+                _semaphore = new Semaphore(1, 1);
+            }
+            //Console.WriteLine($"Created semaphore {name}, created new: {createdNew}");
+            var obtained = _semaphore.WaitOne(TimeSpan.FromMilliseconds(100));
+            if (!obtained)
+            {
+                _semaphore.Dispose();
+                var lockPrefix = isSystemWide ? "global" : "local";
+                throw new InvalidOperationException($"Unable to get a {lockPrefix} lock {name}.");
+            }
+        }
+
+        public void Dispose()
+        {
+            _semaphore.Release(1);
+            _semaphore.Dispose();
         }
     }
 }
